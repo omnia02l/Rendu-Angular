8701867024f86ebe1341cf36cc53bcd50b0cd57a
omnia02l <omnia.lahdhiri@esprit.tn>
2024-02-22 22:07:13 +0100

    appartements

---
+++ src/app/apartments/apartments.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Apartment } from '../core/Models/apartment';
+import { Residence } from '../core/Models/residence';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-apartments',
+  templateUrl: './apartments.component.html',
+  styleUrls: ['./apartments.component.css']
+})
+export class ApartmentsComponent {
+  listResidences: Residence[] = [
+    { id: 1, name: "El fel", address: "Borj Cedria", image: "../../assets/images/R1.jpeg" },
+    { id: 2, name: "El yasmine", address: "Ezzahra", image: "../../assets/images/R2.jpg" },
+    { id: 3, name: "El Arij", address: "Rades", image: "../../assets/images/R3.jpg" },
+    { id: 4, name: "El Anber", address: "Manzah 5", image: "../../assets/images/R4.jpg" }
+  ];
+
+  listApartments: Apartment[] = [
+    { id: 1, appartNum: 1, floorNum: 0, surface: 100, terrace: "oui", surfaceTerrace: 20, category: "S+1", description: "Appartement S+1", residence: this.listResidences[0] },
+    { id: 2, appartNum: 2, floorNum: 0, surface: 130, terrace: "non", surfaceTerrace: 0, category: "S+2", description: "Appartement S+2", residence: this.listResidences[0] },
+    { id: 3, appartNum: 3, floorNum: 0, surface: 150, terrace: "oui", surfaceTerrace: 30, category: "S+3", description: "Appartement S+3", residence: this.listResidences[1] }
+  ];
+
+  filteredApartments: Apartment[] = [];
+  likedApartments: Apartment[] = [];
+
+  idResidence!: number;
+  surfaceFilter: number | undefined; 
+
+  constructor(private actR: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.idResidence = Number(this.actR.snapshot.paramMap.get('idR'));
+    this.listApartments = this.getApartmentsByResidence();
+    this.filteredApartments = this.listApartments;
+  }
+
+  getApartmentsByResidence(): Apartment[] {
+    return this.listApartments.filter(ap => ap.residence.id === this.idResidence);
+  }
+
+  likeApartment(apartment: Apartment): void {
+    if (!this.likedApartments.includes(apartment)) {
+      this.likedApartments.push(apartment);
+    }
+  }
+
+  filterApartmentsBySurface(): void {
+    if (this.surfaceFilter !== undefined) {
+      this.filteredApartments = this.listApartments.filter(apartment => apartment.surface === this.surfaceFilter);
+    } else {
+      this.filteredApartments = this.listApartments; 
+    }
+  }
+  
+}
